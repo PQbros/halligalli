bfa2686d5f92cfeaf34565279f81bd7e4211b642
Thungghuan <1092887426@qq.com>
2020-09-05 07:55:40 +0800

    add game files

---
+++ game.py
@@ -0,0 +1,7 @@
+from player import Player
+
+
+class Game:
+    def __init__(self):
+        self.playerList = []
+        self.playerNum = 0
---
+++ player.py
@@ -0,0 +1,3 @@
+class Player:
+    def __init__(self, cards_num):
+        self.cardsNum = cards_num
